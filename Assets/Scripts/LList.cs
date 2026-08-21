0ed3ac2b7dccbd8a4fcff6652126b5da966c175e
shinney-art <shinneyworawaran@gmail.com>
2026-08-21 12:45:12 +0700

    d

--- Assets/Scripts/LList.cs
+++ Assets/Scripts/LList.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using System.Generic.Collections;
 
 public class LList : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<string> messages;
     void Start()
     {
         
